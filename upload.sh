bf4935faceb08b49f4afa69fec3b858018305351
teamriders098 <111563785+teamriders098@users.noreply.github.com>
2022-09-17 16:10:51 +0530

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,10 @@
+git init
+git add .
+git config --global user.email "Jetushack@gmail.com"
+git config --global user.name "Jetushack"
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/Jetushack/toshi.git
+git push -u origin master
+Jetushack@gmail.com
+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
